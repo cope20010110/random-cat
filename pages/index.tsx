1ab427679ac2ab322e0e36fc31d4e793bd707cfa
cope20010110 <kohei.20010110@gmail.com>
2025-03-14 17:07:41 +0900

    文言変更２

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,7 @@ const indexPage: NextPage<Props> = ({ initialImageUrl }) => {
     return (
         <div className={styles.page}>
           <button onClick={handleClick} className={styles.button}>
-            他のにゃんこを見る
+            One more cat
           </button>
           <div className={styles.frame}>
             {loading || <img src={imageUrl} className={styles.img} />}
